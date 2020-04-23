2195341b1c7e8095be1053c9239c8181b7cea8da
mandab749 <54584390+mandab749@users.noreply.github.com>
2020-04-23 14:58:50 -0400

    Update clean_data.py

--- clean_data.py
+++ clean_data.py
@@ -1,9 +1,3 @@
-
-#https://stackoverflow.com/questions/40327859/pyspark-how-to-read-csv-into-dataframe-and-manipulate-it
-
-# load in data
-#political_ads = sc.textFile("/usr/data/facebook/fbpac-ads-en-US.csv")   
-
 import pyspark
 from csv import reader
 import re 
@@ -12,7 +6,6 @@ import datetime
 from datetime import date
 sc = pyspark.SparkContext()
 sql = SQLContext(sc)
-
 my_df = spark.read.csv("/usr/data/facebook/fbpac-ads-en-US.csv", header = True, inferSchema = True, quote = "\"", escape = "\"")
 df1 = my_df.drop('html')
 df2 = df1.drop('targetings').drop('targeting').drop('targetedness')
@@ -21,12 +14,10 @@ df2.show(1)
 rdd = df2.rdd.map(tuple)
 rdd.take(2)
 
+
 def whatis(line):
   return line[0], line[1], line[2], line[3], line[4], line[5], line[6], line[7], line[8], line[9], line[10], line[11], line[12], line[13], line[14], line[15], line[16], line[17], line[18], line[19]
-
 whatis = rdd.map(whatis)
-
-
 #remove hyperfeed_story
 #accounts for columns that don't have "hyperfeed" in them
 def takeline(line):
@@ -37,6 +28,7 @@ def takeline(line):
     else:
             id = id
     return id, line[1], line[2], line[3], line[4], line[5], line[6], line[7], line[8], line[9], line[10], line[11], line[12], line[13], line[14], line[15], line[16], line[17], line[18], line[19]
+
 
 takeline = rdd.map(takeline)
 takeline.take(3)
@@ -60,6 +52,10 @@ def remove_paragraph_tags(line):
 
 remove_tags = takeline.map(remove_paragraph_tags)
 remove_tags.take(1)
+remove_tags_df = remove_tags.toDF()
+remove_tags_df = remove_tags_df.replace('None',None).na.fill('na')
+remove_tags = remove_tags_df.rdd.map(tuple)
+
 
 
 def fix_time(line):
@@ -69,15 +65,31 @@ def fix_time(line):
     en = "en-US" 
     weirdnum = '0.74362338'
     weirdnum1 = 'we become'
+    if created_time == 'na':
+        created_time = '2021-01-01'
+    if any(c.isalpha() for c in created_time):
+        created_time = '2021-01-01'
     if created_time is None:
+        created_time = '2021-01-01'
+    if "." in created_time[:10]:
+        created_time = '2021-01-01'
+    if len(created_time) != 29:
+        created_time = '2021-01-01'
+    if '0.99814938' in created_time:
         created_time = '2021-01-01'
     if url in created_time or en in created_time or weirdnum in created_time or weirdnum1 in created_time or html in created_time:
         created_time = '2021-01-01'
     else:    
         created_time = str(created_time)
     strip_create_time = created_time[:10]
-    datetime_create = datetime.datetime.strptime(strip_create_time, '%Y-%m-%d')
+    if "." in created_time:
+        created_time = '2021-01-01'
+    try:
+        datetime_create = datetime.datetime.strptime(strip_create_time, '%Y-%m-%d')
+    except:
+        datetime_create = datetime.datetime.strptime('2021-01-01', '%Y-%m-%d')
     return line[0], line[1], line[2], line[3], line[4], line[5], datetime_create, line[7], line[8], line[9], line[10], line[11], line[12], line[13], line[14], line[15], line[16], line[17], line[18], line[19]
+
 
 fix1 = remove_tags.map(fix_time)
 
@@ -89,6 +101,8 @@ def fix_time1(line):
     weirdnum = '0.74362338'
     weirdnum1 = 'we become'
     update_time = line[7]
+    if any(c.isalpha() for c in update_time):
+        update_time = '2021-01-01'
     if update_time is None:
             update_time = '2021-01-01'
     if html in update_time or url in update_time or en in update_time or weirdnum in update_time or weirdnum1 in update_time:
@@ -96,12 +110,17 @@ def fix_time1(line):
     else:
             update_time = str(update_time)
     strip_update_time = update_time[:10]
-    datetime_update = datetime.datetime.strptime(strip_update_time, '%Y-%m-%d')
+    try:
+        datetime_update = datetime.datetime.strptime(strip_update_time, '%Y-%m-%d')
+    except:
+        datetime_update = datetime.datetime.strptime('2021-01-01', '%Y-%m-%d')
     return line[0], line[1], line[2], line[3], line[4], line[5], line[6], datetime_update, line[8], line[9], line[10], line[11], line[12], line[13], line[14], line[15], line[16], line[17], line[18], line[19]
 
 
 fixtime1 = fix1.map(fix_time1)
 fixtime1.take(4)
+
+
 
 def fixline(line):
     paid = line[18]
@@ -116,6 +135,7 @@ def fixline(line):
     else:
             paid = paid
     return line[0], line[1], line[2], line[3], line[4], line[5], line[6], line[7], line[8], line[9], line[10], line[11], line[12], line[13], line[14], line[15], line[16], line[17], paid, line[19]
+
 
 fixline = fixtime1.map(fixline)
 fixline.take(10)
@@ -134,6 +154,8 @@ def images_column(line):
       image_url = image_url.replace(quote, '')
       return line[0], line[1], line[2], line[3], line[4], line[5], line[6], line[7], line[8], str(image_url), line[10], line[11], line[12], line[13], line[14], line[15], line[16], line[17], line[18], line[19]
 
+
+
 image_col = fixline.map(images_column)
 image_col.take(1)  
 
@@ -148,51 +170,18 @@ def target_col(line):
     else:
             key = target
     return line[0], line[1], line[2], line[3], line[4], line[5], line[6], line[7], line[8], line[9], line[10], line[11], line[12], key, line[14], line[15], line[16], line[17], line[18], line[19]
-          
+
 
 target = image_col.map(target_col)
 target.take(2)
 
 
-
 def mapper(line):
     return line[18], 1
-
 
 def addFunc(left, right):
     return left + right
 
-  
 target2 = target.map(mapper).reduceByKey(addFunc)
 target2.take(2)  
-
-
-####################################################################################333
-
-result = isinstance(dictline, dict)
- 
-def key_values(line):
-    target = line[13]
-    if isinstance(target,dict):
-            for k,v in target.items():
-                    return k,v
-                    return line[0], line[1], line[2], line[3], line[4], line[5], line[6], line[7], line[8], line[9], line[10], line[11], line[12], key, line[14, line[15], line[16], line[17], line[18], line[19]       
-
-
-
-def key_values(line):
-    target = line[13]
-    if isinstance(target,dict):
-            for k, v in target.items():
-                return k, v
-            key, values = target.items()
-            return key, values
-    else:
-            return target
-        
-target = image_col.map(target_col)
-target.take(2)    
-
-
-#################################################################
 
